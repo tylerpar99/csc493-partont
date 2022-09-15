bbe0a84973dc434650273e4eab5d8639a2d3ef03
Tyler Parton <67805799+tylerpar99@users.noreply.github.com>
2022-09-15 14:11:39 -0400

    Create setup.sh

---
+++ codebase/setup.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=app.py         # app entry point
+export FLASK_ENV=development    # default
+export FLASK_RUN_PORT=8080      # For consistency (python app.py vs flask run)
+export FLASK_RUN_HOST=0.0.0.0   # To allow external routing to the application
